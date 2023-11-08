999054f8bd26fdb8b12244463963793ff04af967
Eissa Purnell <eissamonet@gmail.com>
2023-11-07 19:22:28 -0600

    added mongoose connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/marcoPoloDB');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017=marco-polo');
 
 // Export connection
 module.exports = mongoose.connection;
