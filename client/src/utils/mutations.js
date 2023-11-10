7c838a8fdb52e690f8969fb86c05f366b140c2c3
Eissa Purnell <eissamonet@gmail.com>
2023-11-09 21:48:20 -0600

    changes made

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 
-export const CREATE_LOSTITEMS = gql`
+export const GET_LOST_ITEMS = gql`
     mutation createLostItems($name: String!, $login: String!, $description: String!, $location: String!, $date: String!, $image: String!, $removeItem: Boolean!) {
         createLostItems(name: $name, login: $login, description: $description, location: $location, date: $date, image: $image, removeItem: $removeItem) {
             _id
@@ -14,20 +14,6 @@ export const CREATE_LOSTITEMS = gql`
         }
     }
 `;
-
-export const CREATE_FOUNDITEMS = gql`
-    mutation createFoundItems($name: String!, $login: String!, $description: String!, $location: String!, $date: String!, $image: String!, $removeItem: Boolean!) {
-        createFoundItems(name: $name, login: $login, description: $description, location: $location, date: $date, image: $image, removeItem: $removeItem) {
-            _id
-            name
-            login
-            description
-            location
-            date
-            image
-            removeItem
-        }
-    }
-`;      
+     
 
 
