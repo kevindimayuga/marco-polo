760a6eddacfc907f4f801c349df5e936d5637160
Jacob <jm.ontiveros94@gmail.com>
2023-11-09 20:24:37 -0500

    created LoginForm.jsx

---
+++ client/src/components/LoginForm.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+
+const LoginForm = () => {
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Perform authentication or further actions with the entered data
+        console.log('Email:', email);
+        console.log('Username:', username);
+        console.log('Password:', password);
+
+        // Reset form fields
+        setEmail('');
+        setUsername('');
+        setPassword('');
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Email:
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+            </label>
+            <br />
+
+            <label>
+                Username:
+                <input
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
+            </label>
+            <br />
+
+            <label>
+                Password:
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+            </label>
+            <br />
+
+            <button type="submit">Login</button>
+        </form>
+    );
+};
+
+export default LoginForm;
