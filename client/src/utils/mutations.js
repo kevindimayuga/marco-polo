4dd9b1082baa2f1322acbb3fa740b73f141cc1b9
Eissa Purnell <eissamonet@gmail.com>
2023-11-09 21:58:14 -0600

    changes made to add and remove items

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,8 +1,21 @@
 import { gql } from '@apollo/client';
 
-export const GET_LOST_ITEMS = gql`
+export const ADD_FOUND_ITEM = gql`
     mutation createLostItems($name: String!, $login: String!, $description: String!, $location: String!, $date: String!, $image: String!, $removeItem: Boolean!) {
         createLostItems(name: $name, login: $login, description: $description, location: $location, date: $date, image: $image, removeItem: $removeItem) {
+            _id
+            name
+            description
+            location
+            date
+            image
+        }
+    }
+`;
+
+export const REMOVE_FOUND_ITEM = gql`
+    mutation removeLostItems($name: String!, $login: String!, $description: String!, $location: String!, $date: String!, $image: String!, $removeItem: Boolean!) {
+        removeLostItems(name: $name, login: $login, description: $description, location: $location, date: $date, image: $image, removeItem: $removeItem) {
             _id
             name
             login
@@ -10,10 +23,10 @@ export const GET_LOST_ITEMS = gql`
             location
             date
             image
-            removeItem
         }
     }
 `;
+
      
 
 
