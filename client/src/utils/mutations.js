33737c03aaa3dc364432b4b7940793b6298935c7
Eissa Purnell <eissamonet@gmail.com>
2023-11-09 21:22:33 -0600

    changes made to mutationsjs

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,8 +1,8 @@
 import { gql } from '@apollo/client';
 
 export const CREATE_LOSTITEMS = gql`
-    mutation createLostItems($name: String!, $description: String!, $location: String!, $date: String!, $image: String!) {
-        createLostItems(name: $name, description: $description, location: $location, date: $date, image: $image) {
+    mutation createLostItems($name: String!, $login: String!, $description: String!, $location: String!, $date: String!, $image: String!, $removeItem: Boolean!) {
+        createLostItems(name: $name, login: $login, description: $description, location: $location, date: $date, image: $image, removeItem: $removeItem) {
             _id
             name
             login
@@ -16,8 +16,8 @@ export const CREATE_LOSTITEMS = gql`
 `;
 
 export const CREATE_FOUNDITEMS = gql`
-    mutation createFoundItems($name: String!, $description: String!, $location: String!, $date: String!, $image: String!) {
-        createFoundItems(name: $name, description: $description, location: $location, date: $date, image: $image) {
+    mutation createFoundItems($name: String!, $login: String!, $description: String!, $location: String!, $date: String!, $image: String!, $removeItem: Boolean!) {
+        createFoundItems(name: $name, login: $login, description: $description, location: $location, date: $date, image: $image, removeItem: $removeItem) {
             _id
             name
             login
