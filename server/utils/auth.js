f05a4d2b23ff0e9d9b78b79fa6ec78a548db82de
Eissa Purnell <eissamonet@gmail.com>
2023-11-13 19:06:03 -0600

    edits made to auth

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,10 +1,17 @@
-const withAuth = (req, res, next) => {
-    // If the user is not logged in, redirect the request to the login route
-    if (!req.session.logged_in) {
-        res.redirect('/login');
-    } else {
-        next();
-    }
+const { GraphQLError } = require('graphql');
+const jwt = require('jsonwebtoken');
+
+const secret = 'mysecretssshhhhhhh';
+const expiration = '2h';
+
+module.exports = {
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
+  signToken: function ({ email, name, _id }) {
+    const payload = { email, name, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
 };
-//exports withAuth
-module.exports = withAuth;
