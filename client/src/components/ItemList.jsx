03bdbc6fbf709ba8138014e3254fa154c29e5d57
Jacob <jm.ontiveros94@gmail.com>
2023-11-14 19:31:39 -0500

    debugging

--- client/src/components/ItemList.jsx
+++ client/src/components/ItemList.jsx
@@ -1,10 +1,10 @@
-export default function itemList({ items }) {
+export default function itemList({ item }) {
     return (
       <div className="container">
         <h1>List of Items:</h1>
         <ul className="list-group">
           {/* Here we use the map method to iterate through each item and return a new array of listed items*/}
-          {items.map((items) => (
+          {item.map((item) => (
             <li className="list-group-item" key={items.login.uuid}>
               {`${items.name}`}
             </li>
